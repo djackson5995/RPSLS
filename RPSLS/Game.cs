118bfa26d1db516ee7a48992d7b628d080d6eebd
djackson5995 <146674106+djackson5995@users.noreply.github.com>
2024-02-04 00:22:45 -0700

    created rules for the game

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -22,6 +22,16 @@ namespace RPSLS
         public void WelcomeMessage()
         {
             Console.WriteLine("Welcome to RPSLS! Here are the rules:\n");
+            Console.WriteLine("Rock crushes Scissors");
+            Console.WriteLine("Scissors cuts Paper");
+            Console.WriteLine("Paper covers Rock");
+            Console.WriteLine("Rock crushes Lizard");
+            Console.WriteLine("Lizard poisons Spock");
+            Console.WriteLine("Spock smashes Scissors");
+            Console.WriteLine("Scissors decapitates Lizard");
+            Console.WriteLine("Lizard eats Paper");
+            Console.WriteLine("Paper disproves Spock");
+            Console.WriteLine("Spock vaporizes Rock\n");
         }
 
         public int ChooseNumberOfHumanPlayers()
